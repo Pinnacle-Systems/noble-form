ad956aff8e1ab227f40fdac34532109beb3d4c49
logesh <logeshsmart72@gmail.com>
2025-05-27 21:29:33 +0530

    commit

--- client/src/constants/apiUrl.js
+++ client/src/constants/apiUrl.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.1.43:8000'
-// export const BASE_URL = '/'
+// export const BASE_URL = 'http://192.168.1.43:8000'
+export const BASE_URL = '/'
 
 
 export const EMP_API =  "emp"
